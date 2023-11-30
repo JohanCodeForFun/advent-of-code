fe1f921cf201d932dbc1d71ab258c55724e089ce
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-11-30 14:55:00 +0100

    setup day 00 to read input file

--- Rust/2023/day-00/src/bin/part1.rs
+++ Rust/2023/day-00/src/bin/part1.rs
@@ -1,9 +1,13 @@
 // problem from 2022, day 1
+use std::fs;
+use std::fs::read_to_string;
 
 fn main() {
-    let input = include_str!("./input1.txt");
+    let input = include_str!("input1.txt");
     let output = part1(input);
     dbg!(output);
+
+    println!("{}", input)
 }
 
 fn part1(input: &str) -> String {
@@ -15,7 +19,7 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        let result = part1("");
+        let result = part1("000");
         assert_eq!(result, 24000.to_string());
     }
 }
