c16cdfedf5a59c74dfa3c1300424d7dd6d3badee
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-12-01 13:39:49 +0100

    add working sample input solution

--- 2024/javascript/day1/index.js
+++ 2024/javascript/day1/index.js
@@ -1,9 +1,30 @@
-const fs = require('node:fs');
+const { open } = require('node:fs/promises');
 
-fs.readFile('sample_input.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
+const left = [];
+const right = [];
+let distance = 0;
+
+(async () => {
+  const file = await open('sample_input.txt');
+
+  for await (const line of file.readLines()) {
+    left.push(parseInt(line[0]))
+    right.push(parseInt(line[4]))
   }
-  console.log(data);
-});
+
+  left.sort((a, b) => a - b)
+  right.sort((a, b) => a - b)
+
+  for (let i = 0; i < left.length; i++) {
+    console.log(left[i], right[i])
+    if (left[i] > right[i]) {
+      distance += left[i] - right[i];
+    } else if (left[i] < right[i]) {
+      distance += right[i] - left[i];
+    } else {
+      continue;
+    }
+
+    console.log(distance)
+  }
+})();
