b7e0b5a561bae0cca6fe95e9f34674c2d6bf81bb
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-12-02 20:19:08 +0100

    part 1 passed

--- 2024/javascript/day2/part1.js
+++ 2024/javascript/day2/part1.js
@@ -1,73 +1,38 @@
 const fs = require('fs');
 const readline = require('readline');
 
-const fileStream = fs.createReadStream('sample_input.txt');
+const fileStream = fs.createReadStream('exercise_input.txt');
 
 const rl = readline.createInterface({
   input: fileStream,
   crlfDelay: Infinity
 });
 
-let lineNumbers;
 let safeReports = 0;
-let positive;
-let negative;
-
-console.log(fileStream)
-
 
 rl.on('line', (line) => {
-  lineNumbers = line.split(' ')
+  let lineNumbers = line.split(' ').map(Number);
+  let isIncreasing = true;
+  let isDecreasing = true;
 
   for (let i = 1; i < lineNumbers.length; i++) {
-    let prev = parseInt(lineNumbers[i - 1]);
-    let curr = parseInt(lineNumbers[i]);
+    let curr = lineNumbers[i];
+    let prev = lineNumbers[i - 1];
+    let diff = curr - prev;
 
-    for (let j = 0; j < lineNumbers.length; j++) {
-      let counter = 1;
-
-      if ((prev > curr && prev - curr === 2) ||
-        (prev > curr && prev - curr === 1) ||
-        (prev > curr && prev - curr === 0)) {
-          counter++;
-          console.log("counter:", counter)
-        } else {
-          console.log("break positive")
-          break;
-        }
-        
-        if (counter === lineNumbers.length) {
-        console.log("----- positive -----")
-        positive = true;
-      }
+    if (diff < 1 || diff > 3) {
+      isIncreasing = false;
     }
-
-    for (let k = 0; k < lineNumbers.length; k++) {
-      if (prev < curr) {
-        negative = false;
-      } else {
-        console.log("break negative")
-        break;
-      }
+    if (diff > -1 || diff < -3) {
+      isDecreasing = false;
     }
   }
 
-    // console.log("positive:", positive)
-    // console.log("negative:", negative)
-
-    
-    // console.log(prev, curr)
-  if (positive) {
-    console.log("true") 
-    safeReports += 1; 
-  } else if (!negative) {
-    console.log("false")
+  if (isIncreasing || isDecreasing) {
+    safeReports += 1;
   }
-  // console.log("pow", lineNumbers)
-  console.log("safe reports:", safeReports)
 });
 
-
 rl.on('close', () => {
-  console.log('File reading completed.');
+  console.log(`Number of safe reports: ${safeReports}`);
 });
